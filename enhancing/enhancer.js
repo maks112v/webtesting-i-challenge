c15eeab7e1549861d5b9a7c1a803250c12e1df81
Maksim <maks112v@gmail.com>
2019-04-08 16:32:36 -0400

    finished mvp and started stretch

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,21 +2,29 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
-  return { ...item };
+  return {
+    ...item,
+    enhancement: item.enhancement + 1 >= 20 ? 20 : item.enhancement + 1
+  };
 }
 
 function fail(item) {
-  return { ...item };
+  return { ...item,
+    durability: item.enhancement < 15 ? item.durability - 5 : item.durability - 10,
+    enhancement: item.enhancement > 15 ? item.enhancement - 1 : item.enhancement,
+   };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
-  return { ...item };
+  return { ...item,
+    name: item.enhancement > 0 ? `[+${item.enhancement}] ${item.name}` : item.name
+   };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,77 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+describe("enhancer.js", () => {
+  describe("enhancer", () => {
+    describe("repair()", () => {
+      it("restore to 100 damage", () => {
+        expect(
+          enhancer.repair({ name: "Sword", durability: 40, enhancement: 0 })
+        ).toEqual({
+          name: "Sword",
+          durability: 100,
+          enhancement: 0
+        });
+      });
+      it("restore to 100 damage", () => {
+        expect(
+          enhancer.repair({ name: "Sword", durability: 10, enhancement: 0 })
+        ).toEqual({
+          name: "Sword",
+          durability: 100,
+          enhancement: 0
+        });
+      });
+    });
+    describe("succeed()", () => {
+      it("succeed to enhance 14", () => {
+        expect(
+          enhancer.succeed({ name: "Sword", durability: 100, enhancement: 0 })
+        ).toEqual({ name: "Sword", durability: 100, enhancement: 1 });
+      });
+      it("Not go over 20", () => {
+        expect(
+          enhancer.succeed({ name: "Sword", durability: 100, enhancement: 20 })
+        ).toEqual({ name: "Sword", durability: 100, enhancement: 20 });
+      });
+      it("succeed to enhance 15", () => {
+        expect(
+          enhancer.succeed({ name: "Sword", durability: 100, enhancement: 14 })
+        ).toEqual({ name: "Sword", durability: 100, enhancement: 15 });
+      });
+      it("succeed to enhance 11", () => {
+        expect(
+          enhancer.succeed({ name: "Sword", durability: 100, enhancement: 10 })
+        ).toEqual({ name: "Sword", durability: 100, enhancement: 11 });
+      });
+    });
+    describe("failed()", () => {
+      it("failed to enhance to 13", () => {
+        expect(
+          enhancer.fail({ name: "Sword", durability: 100, enhancement: 12 })
+        ).toEqual({ name: "Sword", durability: 95, enhancement: 12 });
+      });
+      it("failed to enhance to 20", () => {
+        expect(
+          enhancer.fail({ name: "Sword", durability: 100, enhancement: 20 })
+        ).toEqual({ name: "Sword", durability: 90, enhancement: 19 });
+      });
+    });
+    describe("get()", () => {
+      it("get item with 0", () => {
+        expect(
+          enhancer.get({ name: "Sword", durability: 100, enhancement: 0 })
+        ).toEqual({ name: "Sword", durability: 100, enhancement: 0 });
+      });
+      it("get item with 10", () => {
+        expect(
+          enhancer.get({ name: "Sword", durability: 100, enhancement: 10 })
+        ).toEqual({ name: "[+10] Sword", durability: 100, enhancement: 10 });
+      });
+      it("get item with 20", () => {
+        expect(
+          enhancer.get({ name: "Sword", durability: 100, enhancement: 20 })
+        ).toEqual({ name: "[+20] Sword", durability: 100, enhancement: 20 });
+      });
+    });
+  });
+});
